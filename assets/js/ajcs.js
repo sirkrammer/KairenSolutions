358565beef76e9e0c65dcea6c5fc4e687a397556
Winkawax <chimmy.chimz@gmail.com>
2018-03-20 21:43:33 +0800

    Minor Update

--- assets/js/ajcs.js
+++ assets/js/ajcs.js
@@ -1,167 +1,197 @@
-var ajcs_start = function (view_counter){
+
+account = [
+
+{ username : '',password:'' },
 
 
-      
-        //Flags
-        view_ads = false;
-        ads_list = '';
+];
 
-        // Ajax ViewAds
-        while(!view_ads){
-            jQuery.ajaxSetup({async:false});
-            $.ajax({
-                url: "http://www.mineraview.com/dashboard/res_view_ads.php",
-                error: function(xhr){
-                    console.log( "VIEW ADS FAIL");
-                },
-                success: function(data){
-                    view_ads = true;
-                    ads_list = data;
-                    console.log("VIEW ADS SUCESS");
-                }		
-            });
+account_index = 0;
+account_length = account.length;
+username = '';
 
-        }
+var ajcs_login = function(username,password){
+
+	login = false;
+
+	//Login Credentials
+	$form = $('<form name="frm"></form>');
+	$form.append('<input type="text" name="username" class="form-control" placeholder="Username" maxlength="15" value="'+ username +'">');
+	$form.append('<input type="password" name="password" class="form-control" placeholder="Password" maxlength="15" value="'+ password +'">');
 
 
-        if(ads_list){
-            $form_view_ads = $(ads_list).find('form[name=frm]')
-            $(ads_list).find('.showback button').each( function(index2, value2){
-                jQuery.ajaxSetup({async:false});	
-
-                view = false; 
-                view_result = false;
-
-                console.log("ONCLICK EVENT: " + $(value2).attr("onclick") );
-
-                if ( $(value2).attr("onclick") ) {
-
-                    eval($(value2).attr("onclick").replace("javascript: document.frm.ctr.value","ctr"));	
-
-                    //Login Credentials
-                    $form_view_ads.append('<input type="text" name="ctr" value="'+ ctr +'">');
-
-
-
-
-                    while(!view){
-                        $.ajax({
-                            type: "POST",
-                            url: "http://www.mineraview.com/dashboard/res_view_ads_pay.php",
-                            data:  $form_view_ads.serialize(),
-                            error: function(xhr){
-                                console.log("PAY FAILED");
-                            },
-                            success: function(data){
-                                view = true;
-                                console.log( "PAY SUCESS");
-                            }		
-                        });
-                    }
-
-                    while(!view_result){
-                        $.ajax({
-                            url: "http://www.mineraview.com/dashboard/res_view_ads_process.php",
-                            error: function(xhr){
-                                console.log("PROCESS FAILED");
-                            },
-                            success: function(data){
-                                view_result = true;
-                                console.log( "PROCESS SUCCESS");
-                            }		
-                        });		
-                    }
-
-                }	
-                return index2<view_counter;
-            });
-        }
-
-    
-      ads_status = false;
-
-      // Ajax ViewAds
-      while(!ads_status){
-          jQuery.ajaxSetup({async:false});
-          $.ajax({
-              url: "http://www.mineraview.com/dashboard/res_user.php",
-              error: function(xhr){
-                  console.log( "DASHBOARD FAIL");
-              },
-              success: function(data){
-                  ads_status = true;
-                  dashboard_data = data;
-                  result = " DASHBOARD : " + $(dashboard_data).find(".row.mt").children(".col-md-6.col-sm-6.mb").find("h1").text();
-                  console.log( " DASHBOARD : " + $(dashboard_data).find(".row.mt").children(".col-md-6.col-sm-6.mb").find("h1").text());
-              }		
-          });
-
-      }
-  
-  alert(result);
-
+	//Ajax Login
+	while(!login){
+		jQuery.ajaxSetup({async:false});
+		$.ajax({
+			type: "POST",
+			url: "http://www.mineraview.com/login_process.php",
+			data:  $form.serialize(),
+			error: function(xhr){
+		    	console.log( "LOGIN FAIL");
+			},
+			success: function(data){
+				console.log( "LOGIN SUCCESS");
+				login = true;
+			}		
+		});
+	}
 }
 
-var ajcs_status = function(){
-      result = "";
-  
-      ads_status = false;
-
-      // Ajax ViewAds
-      while(!ads_status){
-          jQuery.ajaxSetup({async:false});
-          $.ajax({
-              url: "http://www.mineraview.com/dashboard/res_user.php",
-              error: function(xhr){
-                  console.log( "DASHBOARD FAIL");
-              },
-              success: function(data){
-                  ads_status = true;
-                  dashboard_data = data;
-                  result = " DASHBOARD : " + $(dashboard_data).find(".row.mt").children(".col-md-6.col-sm-6.mb").find("h1").text();
-                  console.log( " DASHBOARD : " + $(dashboard_data).find(".row.mt").children(".col-md-6.col-sm-6.mb").find("h1").text());
-              }		
-          });
-
-      }
-  
-    alert(result);
-}
 
 var ajcs_logout = function(){
-  
+	logout = false;
+
+	// Ajax Logout
+	while(!logout){
+		jQuery.ajaxSetup({async:false});
+		$.ajax({
+			url: "http://www.mineraview.com/dashboard/logoff.php",
+			error: function(xhr){
+		    	console.log( "LOGOFF FAIL");
+			},
+			success: function(data){
+				logout = true;
+				console.log( "LOGOFF SUCCESS");
+			}		
+		});
+	}
 }
 
+var ajcs_getAdsView = function(){
 
-var ajcs_bank = function(bank_ctr,acctno,acctname,branch,verbose){
-          
-      $form = $('<form class="form-horizontal style-form" method="post" name="frm"></form>');
-      $form.append('<input name="bank_ctr" type="text" id="bank_acctno" class="form-control" value="4">');
-      $form.append('<input name="bank_acctno" type="text" id="bank_acctno" class="form-control" value="' + acctno + '">');
-      $form.append('<input name="bank_acctname" type="text" id="bank_acctname" class="form-control" value="' + acctname + '">');
-      $form.append('<input name="bank_branch" type="text" id="bank_branch" class="form-control" value="' + branch + '">');
-      $form.append('<input name="bank_swift" type="text" id="bank_swift" class="form-control" value="NA">');
-      $form.append('<input type="hidden" name="allow" value="YES">');
-  
+	//Flags
+	view_ads = false;
 
-      bank_update = false;
-                   
-      //Ajax Login
-      while(!bank_update){
-          jQuery.ajaxSetup({async:false});
-          $.ajax({
-              type: "POST",
-              url: "http://www.mineraview.com/dashboard/res_bank.php",
-              data:  $form.serialize(),
-              error: function(xhr){
-                  console.log( "BANK UPDATE FAILED");
-              },
-              success: function(data){
-                  console.log( "BANK UPDATE SUCCESS");
-                  console.log(data)
-                  bank_update = true;
-              }		
-          });
-      }
-  // ajcs_bank(4,'Clarisa Rodriguez','004830372403','CAVITE - IMUS AGUINALDO HIGHWAY',1)
+	// Ajax ViewAds
+	while(!view_ads){
+		jQuery.ajaxSetup({async:false});
+		$.ajax({
+			url: "http://www.mineraview.com/dashboard/res_view_ads.php",
+			error: function(xhr){
+		    	console.log( "VIEW ADS FAIL");
+			},
+			success: function(data){
+				view_ads = true;
+				view_list = data;
+				console.log("VIEW ADS SUCESS");
+			}		
+		});
+		
+	}
+
+	return view_list;
 }
+
+var ajcs_subpostViewAds = function(item){
+	view = false; 
+
+	eval($(item).attr("onclick").replace("javascript: document.frm.ctr.value","ctr"));	
+	
+	//Login Credentials
+	$form_view_ads.append('<input type="text" name="ctr" value="'+ ctr +'">');
+	
+
+	while(!view){
+		$.ajax({
+			type: "POST",
+			url: "http://www.mineraview.com/dashboard/res_view_ads_pay.php",
+			data:  $form_view_ads.serialize(),
+			error: function(xhr){
+				console.log( username + " view request failed.");
+			},
+			success: function(data){
+				view = true;
+				console.log( (new Date()).toLocaleTimeString() + ' ' + username + ' is now viewing ads ' + ctr + '...');
+			}		
+		});
+	}
+
+	return true;
+}
+
+var ajcs_postAdsView = function(view_list,view_count = 0,view_index = 0){
+
+	if(view_list){
+		$form_view_ads = $(view_list).find('form[name=frm]')
+
+		view_count = view_count;
+		view_button_index = view_index;
+		view_button_items = $(view_list).find('.showback button');
+
+		if ( $(view_button_items[view_button_index]).attr("onclick") ) {
+			ajcs_subpostViewAds(view_button_items[view_button_index]);
+		}
+
+		// Set view timer
+		var view_engine = setTimeout( function(){ 
+			view_result = false;
+
+			while(!view_result){
+				$.ajax({
+					url: "http://www.mineraview.com/dashboard/res_view_ads_process.php",
+					error: function(xhr){
+						console.log("PROCESS FAILED");
+					},
+					success: function(data){
+						view_result = true;
+						console.log( (new Date()).toLocaleTimeString() + ' Finished viewing ads...');
+					}		
+				});		
+			} 
+
+			view_count++;
+			view_button_index++;
+
+			if(view_count >= view_button_items.length){
+				view_engine = null;
+				ajcs_getAdsStatus();
+				ajcs_logout();
+
+				// move to next account;
+				account_index++;
+
+				if(account_index < account_length){
+					ajcs_init(account_index);
+				}
+				
+			} else {
+				view_engine = null;
+				ajcs_postAdsView(view_list,view_count,view_button_index);
+			}
+
+		},600000);
+
+	}
+
+}
+
+var ajcs_getAdsStatus = function(){
+	ads_status = false;
+
+	// Ajax ViewAds
+	while(!ads_status){
+		jQuery.ajaxSetup({async:false});
+		$.ajax({
+			url: "http://www.mineraview.com/dashboard/res_user.php",
+			error: function(xhr){
+		    	console.log( "DASHBOARD FAIL");
+			},
+			success: function(data){
+				ads_status = true;
+				dashboard_data = data;
+				console.log( " DASHBOARD : " + $(dashboard_data).find(".row.mt").children(".col-md-6.col-sm-6.mb").find("h1").text());
+			}		
+		});
+		
+	}
+
+}
+
+var ajcs_init = function(index){
+	username = account[index].username;
+	ajcs_login(account[index].username,account[index].password);
+	view_list = ajcs_getAdsView();
+	ajcs_postAdsView(view_list);	
+}
+
